7206eda484d1fb00949092cc74b2d105840f97fc
Kishore Sripada <kishore.sripada@sensis.com.au>
2019-11-17 18:32:47 +1100

    Enhanced test cases with exceptions

--- src/main/java/com/momenton/OrganisationStructureMain.java
+++ src/main/java/com/momenton/OrganisationStructureMain.java
@@ -1,15 +1,18 @@
 package com.momenton;
 
 import com.momenton.beans.Employee;
-import java.util.logging.Logger;
 import com.momenton.beans.Company;
+import com.momenton.exceptions.EmployeeDataException;
+import com.momenton.exceptions.NoManagerException;
 
 public class OrganisationStructureMain {
 
-    private static final Logger logger = Logger.getLogger(OrganisationStructureMain.class.getName());
-
     public static void main(String[] args) {
         Company company = new Company();
-        company.addEmployee(new Employee(150, null, "Jamie", true));
+        try {
+            company.addEmployee(new Employee(150, null, "Jamie", true));
+        } catch (EmployeeDataException | NoManagerException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/test/java/com/momenton/CompanyTest.java
+++ src/test/java/com/momenton/CompanyTest.java
@@ -2,6 +2,8 @@ package com.momenton;
 
 import com.momenton.beans.Company;
 import com.momenton.beans.Employee;
+import com.momenton.exceptions.EmployeeDataException;
+import com.momenton.exceptions.NoManagerException;
 import org.junit.Test;
 import org.junit.Before;
 import static org.junit.Assert.assertThat;
@@ -12,19 +14,15 @@ public class CompanyTest {
 	private Company company = null;
 
 	@Before
-	public void setUp() {
+	public void setUp()  throws EmployeeDataException, NoManagerException {
 		company = new Company();
-	}
-
-	@Test
-	public void should_create_a_valid_ceo() {
 		company.addEmployee(new Employee(1, null, "Jamie", true));
 		assertThat(company.getEmployeesList().size(), is(1));
 	}
-    
-    @Test
-	public void should_create_a_valid_employee() {
+
+	@Test
+	public void should_create_a_valid_employee()  throws EmployeeDataException, NoManagerException {
 		company.addEmployee(new Employee(2, 1, "Alan", false));
-		assertThat(company.getEmployeesList().size(), is(1));
+		assertThat(company.getEmployeesList().size(), is(2));
 	}
 }

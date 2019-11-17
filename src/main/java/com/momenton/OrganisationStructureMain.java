7b20085a2345f4b39939833a2f5dc296200d4a7f
Kishore Sripada <kishore.sripada@sensis.com.au>
2019-11-17 18:39:00 +1100

    Add all employees in main class for testing purposes

--- src/main/java/com/momenton/OrganisationStructureMain.java
+++ src/main/java/com/momenton/OrganisationStructureMain.java
@@ -11,6 +11,11 @@ public class OrganisationStructureMain {
         Company company = new Company();
         try {
             company.addEmployee(new Employee(150, null, "Jamie", true));
+            company.addEmployee(new Employee(100, 150, "Alan", false));
+            company.addEmployee(new Employee(220, 100, "Martin", false));
+            company.addEmployee(new Employee(275, 100, "Alex", false));
+            company.addEmployee(new Employee(400, 150, "Steve", false));
+            company.addEmployee(new Employee(190, 400, "David", false));
         } catch (EmployeeDataException | NoManagerException e) {
             e.printStackTrace();
         }
